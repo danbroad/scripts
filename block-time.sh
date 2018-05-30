9c785e5f391f5e6b0f8d4b95d761f326e3790bda
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2018-05-30 21:17:01 +0100

    Update block-time.sh

--- block-time.sh
+++ block-time.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-TIMESTAMP="$(/opt/JungleTestnet-panthereos42/cleos.sh get table eosio eosio producers -l 150 | grep panthereos42 -A 8 | grep last_produced_block | cut -d':' -f2)"
+DIR=/opt/JungleTestnet-panthereos42
+PRODUCER=panthereos42
+TIMESTAMP="$($DIR/cleos.sh get table eosio eosio producers -l 150 | grep $PRODUCER -A 8 | grep last_produced_block | cut -d':' -f2)"
 if [ "$(uname)" == "Darwin" ]; then
     date -d "$((($TIMESTAMP * 500 + 946684800000) / 1000))"      
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
